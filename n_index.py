aa1b7266867be7878afd5806df8f47ff022a118f
dsc381 <dcohen@cs.umass.edu>
2015-10-20 22:25:08 -0400

    created index class and implemented n_k counter for katz model

--- n_index.py
+++ n_index.py
@@ -1,40 +1,61 @@
 
 import os,sys,pickle
 import marisa_trie
-from nltk import word_tokenize
+from nltk import word_tokenize,bigrams
+from collections import Counter
 
-def counts_creator():
-    """accepts trie, creates list of grams with occurrences > k and < k
-    returns > k list, and < k list"""
+class Index:
+    def __init__(self,mypath,stem=None):
+        #if no trie index has been created, create then load it
+        if 'trie_dict.pkl' not in os.listdir(mypath):
+            self.build_all(mypath,stem)
+        self.trie = pickle.load(open(mypath,"rb"))
 
+    def build_all(mypath,stem):
+        """mypath is path to where files are stored"""
+        t_dict = {}
+        files = [f for f in os.listdir(mypath) if isfile(join(mypath,f))]
+        for fil in files:
+            f = open(fil,'r')
+            corpus = word_tokenize(f.read())
+            if stem != None:
+                corpus = stem(corpus)
+            t_dict = {fil:build_trie(corpus)}
+            f.close()
+        with open('trie_dict.pkl','wb') as handle:
+            pickle.dump(t_dict,handle)
 
-def good_turing_smoothing():
-    """accepts trie, calculated smoothed counts and adds them to trie"""
+    def get_grams(corpus,k):
+        """accepts tokenized corpus, returns dict of gram:(count)"""
+        bgs = nltk.bigrams(corpus)
+        tgs = nltk.trigrams(corpus)
+        ugs = corpus
+        bgc,tgc,ugc = map(nltk.FreqDist,[bgs,tgs,ugs])
+        bgd, tgd, ugd = map(good_turing_smoothing,[bgc,tgc,ugc])
+        #time efficient hash combining
+        final_dict = dict(bgd, **tgd)
+        final_dict.update(ugd)
 
+        #get nk+1 and n1 counts for each gram for katz mode
+        nks = map(get_nk_counts,[ugc,bgc,tgc])
+        return final_dict,nks
 
-def build_trie():
-    """accepts TREC style corpus, build trie for each doc"""
-    all_grams  = get_grams(corpus,3)
+    def build_trie():
+        """accepts TREC style corpus, build trie for each doc"""
+        all_grams,k_counts  = get_grams(corpus,3)
 
-    #format of storage is (uint,uint) in  byte format
-    fmt = "<II"
-    trie = marisa_trie.RecordTrie(fmt,all_grams.items())
-    return trie
+        #format of storage is (uint,uint) in  byte format
+        fmt = "<II"
+        trie = marisa_trie.RecordTrie(fmt,all_grams.items())
+        return trie
 
-def build_all(mypath,stem=None):
-    """mypath is path to where files are stored"""
-    t_dict = {}
-    files = [f for f in os.listdir(mypath) if isfile(join(mypath,f))]
-    for fil in files:
-        f = open(fil,'r')
-        corpus = word_tokenize(f.read())
-        if stem != None:
-            corpus = stem(corpus)
-        t_dict = {fil:build_trie(corpus)}
-        f.close()
+    def get_nk_counts(self,bg):
+        nk = []
+        counts = gt.countOfCountsTable(bg)
+        nk.append[counts[1]]
+        nk.append[counts[k+1]]
+        return nk
 
-    with open('trie_dict.pkl','wb') as handle:
-        pickle.dump(a,handle)
 
 
 
